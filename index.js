4ffca294c0ac99f4d481da62f2e8beaeef0192eb
TabithaKoech <tabbyk1718@gmail.com>
2022-06-19 09:51:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for (let n = 0; n < array.length; n++) {
+    const testValue = target - array[n];
+    for (let m = n + 1; m < array.length; m++) {
+        if (array[m] === testValue) {
+            return true;
+        };
+    };
+};
+return false;
+};
+
 
 /* 
   Write the Big O time complexity of your function here
+   Runtime: O(n^2)
+   Space: O(n)
 */
+
 
 /* 
   Add your pseudocode here
+  iterate through the array, and if it finds a value that is equal to target 
+  check if a value exists in an array
+  iterating through the array, and checking if the two values match
+  If it is, then return true; otherwise return false
 */
 
 /*
   Add written explanation of your solution here
+  The function should return true if any pair of numbers in the array adds up to the target number.
 */
 
 // You can run `node index.js` to view these console logs
